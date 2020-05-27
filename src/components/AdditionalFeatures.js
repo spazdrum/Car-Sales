3be2dff68ee9e80406bf100aca1c59e13cb629bb
spazdrum <spazdrum@gmail.com>
2020-05-26 23:07:12 -0400

    fixed errors

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -7,7 +7,7 @@ const AdditionalFeatures = props => {
   return (
     <div className="content">
       <h4>Additional Features</h4>
-      {props.additionalFeatures.length ? (
+      {props.addFeature.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
             <AdditionalFeature key={item.id} feature={item} addFeature={props.addFeature} />
