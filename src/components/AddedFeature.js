bd233f8d2f95428e6227d512b5cc8cb6788e7a8e
spazdrum <spazdrum@gmail.com>
2020-07-25 00:33:52 -0400

    MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
+import { removeItems } from "../actions/index";
+import { connect } from "react-redux";
 
-const AddedFeature = props => {
+const AddedFeature = (props) => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        className="button"
+        onClick={() => props.removeItems(props.feature)}
+      >
+        X
+      </button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, { removeItems })(AddedFeature);
